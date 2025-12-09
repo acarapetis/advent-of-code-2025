8c4e6d3216661c05b24ab49cd7951326e014b79b
Anthony Carapetis <anthony.carapetis@gmail.com>
2025-12-09 20:47:49 +1100

    day 6 part 1

---
+++ Day6.lean
@@ -0,0 +1,42 @@
+def Matrix (T: Type) (height: Nat) (width: Nat) := Vector (Vector T width) height
+structure Grid  (T: Type) where
+  height: Nat
+  width: Nat
+  data: Matrix T height width
+instance: Inhabited (Grid T) where default := ⟨0, 0, #v[]⟩
+
+def Grid.ofRows [Inhabited T] (rows: Array (Array T)): Grid T :=
+  if let some first := rows[0]? then
+    ⟨rows.size, first.size, Vector.ofFn fun i => Vector.ofFn fun j => rows[i]![j]!⟩
+  else default
+
+def transpose (x: Matrix T a b): Matrix T b a :=
+  Vector.ofFn fun i => Vector.ofFn fun j => x.get j |>.get i
+
+def spaceSplit (s: String): List String := s.splitOn " " |>.filter (· != "")
+
+inductive Op where
+  | Mult : Op
+  | Add : Op
+deriving Inhabited
+
+def parseOp: String -> Op
+| "*" => Op.Mult
+| "+" => Op.Add
+| _ => panic! "bad op string"
+
+def applyOp (op: Op) (data: Vector Nat n): Nat :=
+  let (o, x): (Nat -> Nat -> Nat) × Nat := match op with
+  | Op.Mult => ((· * ·), 1)
+  | Op.Add => ((· + ·), 0)
+  data.toList.foldl o x
+
+def main : IO Unit := do
+  let contents <- IO.FS.readFile "input6.txt"
+  let lines := contents.trim.splitOn "\n"
+  if let opline::datalines := lines.reverse then
+    let ops := spaceSplit opline |>.map parseOp
+    let rows := datalines.map spaceSplit |>.map (·.map String.toNat! |>.toArray) |>.toArray
+    let grid := Grid.ofRows rows |>.data |> transpose
+    let results := ops.zipWith applyOp grid.toList 
+    IO.println <| results.sum
